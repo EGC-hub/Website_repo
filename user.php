057183ab3fb15e5f1f960740030ba710c9393e29
Sadu-sadullah <sadullah2000@gmail.com>
2024-11-11 13:22:07 +0530

    asd

--- user.php
+++ user.php
@@ -10,7 +10,7 @@ $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: ". $conn->connect_error;)
+    die("Connection failed: ". $conn->connect_error):
     // Alternatively, you can redirect to an error page or display a friendly error message
     // header("Location: error-page.php";
     // exit;
@@ -25,6 +25,6 @@ $newHash = password_hash($newPassword, PASSWORD_DEFAULT;)
 
 // Insert the new user details into the database
 $query = "INSERT INTO users (username, password, role) VALUES ('admin', '$newHash', 'admin')";
-mysqli_query($conn, $query;)
+mysqli_query($conn, $query);
 
 ?>
