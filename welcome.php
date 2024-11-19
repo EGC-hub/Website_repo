87a1228b4378e26426f43ce61b849e1892a1cb5b
Sadu-sadullah <sadullah2000@gmail.com>
2024-11-19 15:14:03 +0530

    welcome changes

--- welcome.php
+++ welcome.php
@@ -5,7 +5,7 @@ session_start();
 // Check if the user is logged in
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     // Redirect to login page if not logged in
-    header("Location: login.php");
+    header("Location: portal-login.php");
     exit;
 }
 
