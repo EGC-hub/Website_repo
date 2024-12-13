8a97f1dd4c538a9c227eba3a9ae61b41be161f52
Sadu-sadullah <sadullah2000@gmail.com>
2024-12-13 12:28:18 +0530

    dfasfasd

--- tasks.php
+++ tasks.php
@@ -7,9 +7,9 @@ error_reporting(E_ALL);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
 session_start();
 
