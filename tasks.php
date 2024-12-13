237150c876d5afa5077af0cb406d91df94df4fe2
Sadu-sadullah <sadullah2000@gmail.com>
2024-12-13 13:01:49 +0530

    asfdfsd

--- tasks.php
+++ tasks.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$config = include dirname(__DIR__) . '/config.php';
+$config = include '../config.php'; 
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
