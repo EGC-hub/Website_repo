b0c4c1eb1e41142aeb4e7a09748ed62d45c55f80
Sadu-sadullah <sadullah2000@gmail.com>
2024-12-13 13:18:18 +0530

    asdffsd

--- tasks.php
+++ tasks.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$config = include 'config.php'; 
+$config = include("../config.php");
 
 if ($config === null) {
     die('Config file could not be loaded. Check the path and contents.');
