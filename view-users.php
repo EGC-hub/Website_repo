f2fb206811cc0ff6b65a3972e9a251f13a37792c
Sadu-sadullah <sadullah2000@gmail.com>
2024-12-17 18:25:16 +0530

    asfsda

--- view-users.php
+++ view-users.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-require_once 'config.php'; // Database connection
+require_once '../config.php'; // Database connection
 
 // Check if the user is logged in
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['role'])) {
