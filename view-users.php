8e88bc4084d34d837093ac5ed3981a8adb0b4738
Sadu-sadullah <sadullah2000@gmail.com>
2024-12-19 09:34:27 +0530

    sfs

--- view-users.php
+++ view-users.php
@@ -224,7 +224,7 @@ try {
                                 <td><?= htmlspecialchars($user['role']) ?></td>
                                 <td>
                                     <a href="edit-user.php?id=<?= $user['id'] ?>" class="edit-button">Edit</a>
-                                    <a href="delete-user.php?id=<?= $user['id'] ?>" class="edit-button">Edit</a>
+                                    <a href="delete-user.php?id=<?= $user['id'] ?>" class="edit-button">Delete</a>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
