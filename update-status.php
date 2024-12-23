6a2d07f823d1f66a7d8b8ff6db7fd81190fc6892
Sadu-sadullah <sadullah2000@gmail.com>
2024-12-23 15:39:43 +0530

    19

--- update-status.php
+++ update-status.php
@@ -1,4 +1,13 @@
 <?php
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+exit;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $config = include '../config.php';
 
 // Database connection details
