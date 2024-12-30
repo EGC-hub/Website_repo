dd7b57946846003873aebaa2bf13b7ccfa735e45
Sadu-sadullah <sadullah2000@gmail.com>
2024-12-30 12:31:20 +0530

    13

--- tasks.php
+++ tasks.php
@@ -621,7 +621,7 @@ $result = $stmt->get_result();
                                     <td><?= htmlspecialchars($row['department']) ?></td>
                                 <?php endif; ?>
                                 <td><?= htmlspecialchars(date("d M Y, h:i A", strtotime($row['recorded_timestamp']))) ?></td>
-                                <?php if ($user_role !== 'user' && $row['assigned_by'] == $_SESSION['user_id']): ?>
+                                <?php if ($user_role !== 'user' && $row['assigned_by_id'] == $_SESSION['user_id']): ?>
                                     <td>
                                         <a href="edit-tasks.php?id=<?= $row['task_id'] ?>" class="btn btn-warning btn-sm">Edit</a>
                                         <form method="POST" action="delete-task.php" style="display:inline;">
