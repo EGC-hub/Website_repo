c9530f5aea3974a15595c6a190eb6aaefcfe5fd0
Sadu-sadullah <sadullah2000@gmail.com>
2025-01-15 12:07:23 +0530

    34

--- welcome.php
+++ welcome.php
@@ -339,6 +339,7 @@ try {
 <html lang="en">
 
 <head>
+    <?= $taskDistribution ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
