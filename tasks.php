af33b7835fb3f41d4bd079440e4e36838cfb10f9
Sadu-sadullah <sadullah2000@gmail.com>
2025-01-21 11:58:16 +0530

    5

--- tasks.php
+++ tasks.php
@@ -392,7 +392,7 @@ function getWeekdays($start, $end)
             font-family: Arial, sans-serif;
             background-color: #f4f4f4;
             margin: 0;
-            padding: 20px;
+            padding: 0;
         }
 
         * {
